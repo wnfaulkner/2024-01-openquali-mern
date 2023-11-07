02a1fe7a4f4ff18fc547f21b34a3d233701b680b
wnf <william@fluxrme.com>
2023-11-07 04:16:41 -0600

    adds crud helper

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-
+require('dotenv').config();
+require('./config/database')
 const app = express();
 
 // MIDDLEWARE 1
