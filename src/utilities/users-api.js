49915813ded7c4865efc34a0fd76cfdf8d688c97
wnf <william@fluxrme.com>
2023-11-09 12:41:05 -0600

    corrects refactoring in previous commit

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,42 +1,16 @@
 // users-api.js
 
-// This is the base path of the Express route we'll define
-const BASE_URL = '/api/users';
+import sendRequest from './send-request';
+const BASE_URL = '/api/users'; // This is the base path of the Express route we'll define
 
-export async function signUp(userData) {
-  // Fetch uses an options object as a second arg to make requests other than basic GET requests, include data, headers, etc. 
-  const res = await fetch(BASE_URL, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    // Fetch requires data payloads to be stringified and assigned to a body property on the options object
-    body: JSON.stringify(userData)
-  });
-  
-  console.log(res.json)
-
-  if (res.ok) {
-    // res.json() will resolve to the JWT
-    return res.json();
-  } else {
-    throw new Error('Invalid Sign Up');
-  }
+export function signUp(userData) {
+  return sendRequest(BASE_URL, 'POST', userData);
 }
 
-export async function login (credentials) {
-  const loginUrl = `${BASE_URL}/login`
-  const res = await fetch(
-    loginUrl,
-    {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(credentials)
-    }
-  );
-  //console.log(res.url)
-  if (res.ok) {
-    // res.json() will resolve to the JWT
-    return res.json();
-  } else {
-    throw new Error('Invalid Login');
-  }
+export function login(credentials) {
+  return sendRequest(`${BASE_URL}/login`, 'POST', credentials);
+}
+
+export function checkToken() {
+  
 }
