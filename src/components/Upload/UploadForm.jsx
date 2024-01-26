139150c96ce8b69f1a35e783ef714d94aced54bd
wnf <william@fluxrme.com>
2024-01-25 19:06:23 -0600

    adds form and draft function for uploading files

--- src/components/Upload/UploadForm.jsx
+++ src/components/Upload/UploadForm.jsx
@@ -5,26 +5,52 @@ import './UploadForm.css'
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
-function UploadForm( props ) {
+function UploadFile( props ) {
+  const [filename, setFilename] = useState('')
   const [file, setFile] = useState([{}])
+  // const [status, setstatus] = useState('')
 
-  async function uploadFile() {
-    // const formData = new FormData()
-    // formData.append('file', file)
-    // console.log(file)
-    // const res = await axios.post('/api/upload', formData, {
-    //   headers: {
-    //     'Content-Type': 'multipart/form-data'
-    //   }
-    // })
-    // console.log(res)
+  let api = 'http://localhost:8000/api'
+  const {updateFileList } = props
+
+  const uploadFile = () => {
+    console.log('Save Button clicked!')
+
+    let formData = new FormData();
+    formData.append("pdf", filename)
+
+    let axiosConfig = {
+      headers: {
+        'Content-Type': 'multpart/form-data'
+      }
+    }
+    console.log(formData)
+
+    axios.post(api + '/files/', formData, axiosConfig)
+    .then(
+      response =>{
+        // console.log(response)
+        // setstatus('File Uploaded Successfully')
+      }
+    ).catch(error =>{
+      console.log(error)
+    })
+    .then(updateFileList)
   }
 
   return (
     <div id="upload-interface">
       <p>UPLOAD FORM PLACEHOLDER</p>      
+      <form >
+        <div className="form-group">
+          <label htmlFor="exampleFormControlFile1" className="float-left">Browse</label>
+          <input type="file" onChange={e => setFilename(e.target.files[0])} className="form-control" />
+        </div>
+        <button type="button" onClick={UploadFile} className="btn btn-primary float-left mt-2">Upload</button>
+        {/* {status ? <h2>{status}</h2>:null} */}
+      </form>
     </div>
   )
 }
 
-export default UploadForm
+export default UploadFile
